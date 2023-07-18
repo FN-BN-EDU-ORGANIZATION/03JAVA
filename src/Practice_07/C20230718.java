c446656f0ca541050b0057dd080cdf0ad093cb19
FORYOUCOM <FORYOUCOM@dd>
2023-07-18 23:37:37 +0900

    sad

---
+++ src/Practice_07/C20230718.java
@@ -0,0 +1,79 @@
+package Practice_07;
+
+
+
+
+public class C20230718 {
+	public static void main(String[] args) {
+
+//		@Override
+//		public void actionPerformed(ActionEvent e) {
+	//  ActionEvent를 처리하는 메서드
+//		클릭시 이벤트 처리
+		
+//			if(e.getSource()==login_btn) {
+//				System.out.println("LOGIN_BTN");
+//				String id = id_txt.getText();
+//				String pw = pw_txt.getText();
+//			입력한 아이디 비번 가져옴	
+
+//				
+//				Map<String, Object> param = new HashMap();
+//				param.put("id", id);
+//				param.put("pw", pw);
+//				가져온 아이디 비번 HashMap에 저장
+		
+//				
+//				Map<String, Object> result = controller.execute("/member", 5, param);
+//				controller의 execute 메서드를 가져와서 member 서버에서 param매개변수를 result에 저장
+		
+//				if(result!=null) {
+//				result가 null이 아닐때 참		
+		
+//					JOptionPane.showMessageDialog(null,"로그인에 성공했습니다!", "LogIn",JOptionPane.INFORMATION_MESSAGE);
+//					로그인 성공했을때 "로그인 성공했습니다!" 메시지 나옴
+		
+//					logmaingui = new Log_MainGUI();
+//					logmaingui.setVisible(true);
+//					유저에게 로그인창을 보여줌
+		
+//					this.setVisible(false);
+//					유저에게 로그인창 숨김
+		
+//					this.setUserName(id);
+//					유저의 정보보여줌
+		
+		
+//					
+//				} else{
+//					JOptionPane.showMessageDialog(null,"로그인에 실패했습니다..", "LogIn",JOptionPane.ERROR_MESSAGE);	
+//				로그인 실패할경우 "로그인에 실패했습니다" 메시지 날림
+		
+//					this.Frm_login = new JFrame();
+//				JFrame클래스 새로운 인스턴스 생성
+
+//					Frm_login.setVisible(false);
+//					}
+//				로그인 창 숨김
+		
+//			} else if(e.getSource()==join_btn) {
+//			join_btn눌렀을때 회원가입 창 열어줌
+		
+//				System.out.println("JOIN_BTN");
+//				joinUI = new JoinUI();
+//				joinUI.setVisible(true);
+//				this.setVisible(false);
+//			join_btn을 누르면 회원가입UI띄움
+//			실패할경우 숨김
+		
+//			}
+//		}
+		
+		
+		
+		
+		
+	}	
+		
+	
+}
