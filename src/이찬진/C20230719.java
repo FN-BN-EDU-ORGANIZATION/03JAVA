e8d107afbba9132a274eb15684c4e6646687e001
chanj <chanjini7@naver.com>
2023-07-19 14:32:58 +0900

    20230719

---
+++ src/이찬진/C20230719.java
@@ -0,0 +1,32 @@
+package 이찬진;
+
+public class C20230719 {
+
+	public static void main(String[] args) {
+		
+		
+		//예제1
+		//다음중에서 리터럴,변수,상수,키워드(자료형)을 각각 분리해보기
+		
+		int i = 100;
+		long l = 100L;
+		final float PI = 3.14f;
+		
+//		답 : - 리터럴 : 100, 100L, 3.14f
+//			- 변수 : i, l
+//			- 키워드 : int, long, final, float
+//			- 상수 : PI
+		
+		//예제2
+		//참조형 변수와 같은 크기의 기본형 변수는
+		
+		//a. int b. long c. short d. float e. double
+		
+		//a, d
+		//=> 모든 참조형 변수의 크기는 4byte이다.
+
+
+
+	}
+
+}
