022bf0be0b0082f380c1917f57151bbaa0b7cdeb
chanj <chanjini7@naver.com>
2023-07-21 14:23:14 +0900

    20230721

--- src/이찬진/C20230719.java
+++ src/이찬진/C20230719.java
@@ -12,6 +12,26 @@ public class C20230719 {
 //		long l = 100L;
 //		final float PI = 3.14f;
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 //		답 : - 리터럴 : 100, 100L, 3.14f
 //			- 변수 : i, l
 //			- 키워드 : int, long, final, float
