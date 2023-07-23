432102aa85a45c33f8812a1e8643c67be079398a
FORYOUCOM <FORYOUCOM@dd>
2023-07-23 23:49:46 +0900

    ㅁ

---
+++ src/Practice_07/C20230721.java
@@ -0,0 +1,71 @@
+package Practice_07;
+
+
+
+
+public class C20230721 {
+	public static void main(String[] args) {
+//		챗GPT검색하여 내용가져올수있음
+		
+//		07/23
+
+//		public static void main(String[] args) {
+//	        String searchQuery = "ChatGPT"; // 검색어를 입력하세요
+//	        searchAndPrintResults(searchQuery);
+//	    }
+//		검색어를 받아 웹페이지 검색 결과 출력메서드
+//
+//	    public static void searchAndPrintResults(String query) {
+//	        String searchUrl = "https://www.example.com/search?q=" + query; // 검색할 웹 페이지의 URL을 넣어주세요
+//		Chatgpt 검색하여 가져옴
+		
+//	        try {
+//	            Document doc = Jsoup.connect(searchUrl).get();
+//	            Elements results = doc.select(".search-result"); // 검색 결과를 감싸는 부모 요소를 선택합니다 (이 예제에서는 클래스 이름이 "search-result"인 요소)
+//				URL 저장
+		
+		
+//	            for (Element result : results) {
+//	                Element titleElement = result.selectFirst(".result-title"); // 제목을 가져옵니다 (이 예제에서는 클래스 이름이 "result-title"인 요소)
+//	                Element contentElement = result.selectFirst(".result-content"); // 내용을 가져옵니다 (이 예제에서는 클래스 이름이 "result-content"인 요소)
+//				웹페이지접속 이후 결과를 선택
+//				반복문 사용하여 내용 가져온다 
+		
+//	                if (titleElement != null && contentElement != null) {
+//	                    String title = titleElement.text();
+//	                    String content = contentElement.text();
+//
+//	                    // 원하는 줄바꿈 형식으로 출력하거나 다른 처리를 수행합니다.
+//	                    System.out.println("제목: " + title);
+//	                    System.out.println("내용: " + content);
+//	                    System.out.println("-----------------------");
+//	                }
+//	            }
+//	        } catch (IOException e) {
+//	            e.printStackTrace();
+//	        }
+//	    }
+//	}
+		
+//		검색결과를 출력 -> 자바로 구현(주소, 결과는 바꾸어야한다)
+		
+		
+//		2)
+//		public static void printStarRectangle(int width, int height) {
+//        for (int i = 0; i < height; i++) {
+//            for (int j = 0; j < width; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println(); // 줄바꿈
+//        }
+//    }
+//}
+		
+//		반복문으 사용 하여 사각형 별짓기 가로 세로 길이를 반복하여 계속 출력
+//		마름모 만들기전 별짓기 예행 연습
+		
+		
+	}	
+		
+	
+}
