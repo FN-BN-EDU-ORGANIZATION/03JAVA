304a1879c2235a0ef0366621e95e60be0bf557b2
황다은 <dutlsghkd@naver.com>
2023-07-26 21:26:15 +0900

    20230726

---
+++ src/황다은/C20230726.java
@@ -0,0 +1,53 @@
+package 황다은;
+
+import java.util.Scanner;
+
+public class C20230726 {
+	public static void main(String[] args) {
+		//예제1
+		//사각형의 넓이와 둘레구하는 문제
+		//너비가 12.5이며, 높이가 36.4를 변수에 저장하고, 각 넓이와 둘레를 계산해 변수에 답고 출력
+//		double width=12.5; double height=36.4;
+//		System.out.println("면적 : "+width*height);
+//		System.out.println("둘레 : "+(width+height)*2);
+		
+		//예제2
+		//scanner를 통해 3개의 값을 입력받은 뒤 그 중 가장 큰 수를 찾으시요(삼항연산자사용)
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("첫번째수");
+//		int num1 = sc.nextInt();
+//		System.out.println("두번째수");
+//		int num2 = sc.nextInt();
+//		System.out.println("세번째수");
+//		int num3 = sc.nextInt();
+//		
+//		int max = (num1>num2)?((num1>num3)?num1:num3):((num2>num3)?num2:num3);
+//		System.out.println("가장큰수 : "+max);
+//		sc.close();
+		
+		//예제3
+		//배열을 이용하여 7명의 IQ를 입력받아 최대 IQ와 최소 IQ를 출력하는 프로그램 
+//		Scanner sc = new Scanner(System.in);
+//		int[] IQ = new int[7];
+//		for(int i=0;i<IQ.length;i++) {
+//			System.out.println("IQ 입력하시오");
+//			IQ[i]=sc.nextInt();
+//		}
+//		int max = IQ[0];
+//		int min = IQ[0];
+//		
+//		for(int i = 1;i<IQ.length;i++) {
+//			if(IQ[i]>max) {
+//				max = IQ[i];
+//			}
+//			if(IQ[i]<min) {
+//				min=IQ[i];
+//			}
+//		}
+//		System.out.println("가장 높은 IQ : "+max);
+//		System.out.println("가장 낮은 IQ : "+min);
+		
+		
+	}
+}
