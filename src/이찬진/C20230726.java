9e4f1c5f0a890a1350ccf51ea7be0941d708cbaa
chanj <chanjini7@naver.com>
2023-07-26 14:43:20 +0900

    20230726

---
+++ src/이찬진/C20230726.java
@@ -0,0 +1,33 @@
+package 이찬진;
+
+public class C20230726 {
+
+	public static void main(String[] args) {
+		
+		//예제1
+		// 별찍기 예제
+		
+//		for(int i=1;i<=7;i++) {
+//			for(int j=i;j<=7;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		System.out.println("------------");
+		
+		
+		//예제2
+		
+		//사각형의 면적 구하기
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("가로 : ");
+//		int width = sc.nextInt();
+//		System.out.print("세로 : ");
+//		int height = sc.nextInt();
+//		
+//		int Square = width * height;
+//		System.out.print("사각형 넓이 : " + Square);
+
+	}
+
+}
