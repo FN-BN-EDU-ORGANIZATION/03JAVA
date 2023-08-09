cb1c968083c83a533fae1c64cf71fdaf10d6eb1e
Euzeth <knightcy1@gmail.com>
2023-08-09 19:02:21 +0900

    2023/08/09

---
+++ src/HomeWork/C20230809/C20230809.java
@@ -0,0 +1,33 @@
+package HomeWork.C20230809;
+
+class Par1{
+	void Func() {}	//완성된 메서드
+}
+class Son1 extends Par1{
+	void Func() {System.out.println("Son1에서 자율적으로 재정의한 Func() 호출!");}
+}
+abstract class Par2{
+	abstract void Func();	//추상메서드(미완성된 메서드)
+}
+class Son2 extends Par2{
+
+	@Override
+	void Func() {
+		System.out.println("Son2에서 강제로 재정의한 Func() 호출!");
+	}
+	
+}
+
+public class C20230809 {
+	public static void main(String[] args) {
+		Par1 ob1 = new Par1();	//상위클래스 객체 생성 o
+		Son1 ob2 = new Son1();	//하위클래스 객체 생성 o
+		Par1 ob3 = ob2;			//Upcasting
+		ob3.Func();				//상위 참조변수로 재정의한 해위객체 메서드 접근 o
+		
+		//Par2 ob4 = new Par2();	//추상클래스 객체 생성 x
+		Son2 ob5 = new Son2();		//하위클래스 객체 생성(추상메서드를 재정의해야가능)
+		Par2 ob6 = new Son2();		//Upcasting
+		ob6.Func();
+	}
+}
