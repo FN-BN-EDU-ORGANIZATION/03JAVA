8835c7708a00b7f37e9bd615b328cd722a9f2366
chanj <chanjini7@naver.com>
2023-08-10 16:19:08 +0900

    20230810

--- src/이찬진/C20230808.java
+++ src/이찬진/C20230808.java
@@ -6,10 +6,9 @@ public class C20230808 {
 
 	public static void main(String[] args) {
 
-		
-		//예제1
-		//알람시계
-		
+		// 예제1
+		// 알람시계
+
 //		Scanner sc = new Scanner(System.in);
 //		int h = sc.nextInt();
 //		int m = sc.nextInt();
@@ -31,28 +30,26 @@ public class C20230808 {
 //			}
 //				
 //		}
-		
-		//예제2
-		  Scanner sc = new Scanner(System.in);
-	        int x = sc.nextInt();
-	        int y = sc.nextInt();
-	        sc.close();
 
-	        if(x>= -1000 && x<=1000 && x!=0) {
-	            if (y >= -1000 && y <= 1000 && y != 0) {
-	                if(x>0 && y>0)
-	                    System.out.println("1");
-	                else if(x<0 && y>0)
-	                    System.out.println("2");
-	                else if(x<0 && y<0)
-	                    System.out.println("3");
-	                else if(x>0 && y<0)
-	                    System.out.println("4");
-	            }
-	        }
-		
-		
-		
+		// 예제2
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		sc.close();
+
+		if (x >= -1000 && x <= 1000 && x != 0) {
+			if (y >= -1000 && y <= 1000 && y != 0) {
+				if (x > 0 && y > 0)
+					System.out.println("1");
+				else if (x < 0 && y > 0)
+					System.out.println("2");
+				else if (x < 0 && y < 0)
+					System.out.println("3");
+				else if (x > 0 && y < 0)
+					System.out.println("4");
+			}
+		}
+
 	}
 
 }
